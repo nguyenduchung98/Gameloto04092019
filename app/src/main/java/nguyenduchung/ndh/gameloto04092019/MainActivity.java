bbb20ff8561ba266cccbccf7b5af3e1118561e4f
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-09-25 21:16:46 +0700

    C

--- app/src/main/java/nguyenduchung/ndh/gameloto04092019/MainActivity.java
+++ app/src/main/java/nguyenduchung/ndh/gameloto04092019/MainActivity.java
@@ -3,35 +3,55 @@ package nguyenduchung.ndh.gameloto04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    Button btnRamdom;
-    TextView txtResult;
-    EditText edtmin,edtmax;
+    Button mbtnRandom;
+    TextView mtxtResult;
+    EditText medtmin,medtmax;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //rule,graph
         initView();
+        //rule,graph
+
         //Task check valid muber
             //1: phai nhap gia tri
             //2: neu so mim>max=> max=min +1
             //3: so min >0
         //Task handle ramdom
+        // (max - min +1) + min
         //Task show toast
          //thong bao loi hay thanh cong
 
+//            Random random = new Random();
+//            int numberint= random.nextInt(10 - 5 + 1) + 5;
+//            Log.d("BBB",numberint +"");
+
+       // cach lay gia tri
+        mbtnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("BBB","da click");
+            }
+        });
+        String sTextMin=medtmin.getText().toString();
+
+
+
     }
     private void initView() {
-        btnRamdom.findViewById(R.id.btn_ramdom);
-        txtResult.findViewById(R.id.txt_Result);
-        edtmin.findViewById(R.id.edt_min);
-        edtmax.findViewById(R.id.edt_max);
+        mbtnRandom=findViewById(R.id.btn_random);
+        mtxtResult=findViewById(R.id.txt_Result);
+        medtmin=findViewById(R.id.edt_min);
+        medtmax=findViewById(R.id.edt_max);
     }
-
 
 }
