03fec86bbb35b37a08dea27a8c0462697ff76f25
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-09-27 20:59:54 +0700

    E

--- app/src/main/java/nguyenduchung/ndh/gameloto04092019/MainActivity.java
+++ app/src/main/java/nguyenduchung/ndh/gameloto04092019/MainActivity.java
@@ -16,8 +16,10 @@ public class MainActivity extends AppCompatActivity {
     Button mbtnRandom;
     TextView mtxtResult;
     EditText medtmin,medtmax;
+    Random mRandom=new Random();
+    String mStringvalueRandom="";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
@@ -26,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         //Task check valid muber
             //1: phai nhap gia tri
             //2: neu so mim>max=> max=min +1
-            //3: so min >0
+            //3: min <100 ,max <=100
         //Task handle ramdom
         // (max - min +1) + min
         //Task show toast
@@ -43,15 +45,36 @@ public class MainActivity extends AppCompatActivity {
                 //lay gia tri edittext
                 String sTextMin=medtmin.getText().toString();
                 String sTextMax=medtmax.getText().toString();
+
                 //kiem tra gia tri
-                if(sTextMin.equals("") || sTextMax.isEmpty()){
-                    Toast.makeText(MainActivity.this,"Chua nhap gia tri",Toast.LENGTH_LONG).show();
+                if(sTextMin.isEmpty() && sTextMax.isEmpty()) return;
+                int sMin=Integer.parseInt(sTextMin);
+                int sMax=Integer.parseInt(sTextMax);
+                if(sMin < 100 && sMax <= 100) {
+                    if (sMin >= sMax) {
+                        sMax = sMin + 1;
+                    }
+                }else {
+                        sMax = 100;
+                        sMin = 99;
+                    }
+                medtmin.setText(sMin + "");
+                medtmax.setText(sMax + "");
+                int valuerabdom =mRandom.nextInt(sMax - sMin + 1) + sMin;
+                mStringvalueRandom += valuerabdom + " - ";
+                mtxtResult.setText(mStringvalueRandom);
+
                 }
 
-            }
         });
 
 
+    }
+
+    private void soLoto(int min,int max){
+        Random random = new Random();
+        int numberint= random.nextInt(max - min + 1) + min;
+        mtxtResult.setText(numberint);
     }
     private void initView() {
         mbtnRandom=findViewById(R.id.btn_random);
